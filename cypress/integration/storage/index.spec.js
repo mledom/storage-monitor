8da148e5b8f13b862318b39091c3034511fd25a2
Matt Ledom <mledom@eletype.com>
2021-04-20 12:26:31 -0400

    fix

--- cypress/integration/storage/index.spec.js
+++ cypress/integration/storage/index.spec.js
@@ -1,20 +1,21 @@
 /// <reference types="cypress" />
 
-describe("Cold Storage Monitor", () => {
+describe('Cold Storage Monitor', () => {
     const mainPage = 'http://10.10.10.19';
     //const mainPage = "http://google.com";
-  it("Visits the Main Dashboard", () => {
-    cy.viewport(1000, 1000);
-    cy.visit(mainPage);
-    cy.wait(2000);
-    cy.contains('Sensaphone Web600');
-    const content = cy.get("#stable td.info").should(($p) => {
-      expect($p).to.be.greaterThan(0);
-      return $p;
+    it('Visits the Main Dashboard', () => {
+        cy.viewport(1000, 1000);
+        cy.visit(mainPage);
+        cy.wait(2000);
+        cy.contains('Sensaphone Web600');
+        const content = cy.get('#stable td.info').should(($p) => {
+            expect($p.length()).to.be.greaterThan(0);
+            return $p;
+        });
+        const date = new Date().toISOString().slice(0, 10);
+        cy.screenshot(date);
+        const file = `./cypress/screenshots/storage/index.spec.js/${date}.png`;
+        console.log(file);
+        cy.task('email', { file });
     });
-    const date = new Date().toISOString().slice(0, 10);
-    cy.screenshot(date);
-    const file = `./cypress/screenshots/storage/index.spec.js/${date}.png`;
-    cy.task('email', { file })
-  });
 });
